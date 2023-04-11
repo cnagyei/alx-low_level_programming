3e19678067e26ccc846d592d1410520b6854a78e
cnagyei <cnagyei15@gmail.com>
2023-04-10 23:27:52 -0700

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string.
+ *
+ * @s: The string to be reversed.
+ *
+ * Return: Void.
+ */
+void rev_string(char *s)
+{
+	char a[100];
+	char *cpy;	/* Pointer to copy string to */
+	int len;
+	char *p = s;
+
+	cpy = &a[0];
+
+	while (*p != '\0')
+		p++;
+	len = p - s;
+
+	/* Create a copy */
+	while ((*cpy++ = *s++))
+		;
+
+	/* Move s back to */
+	s -= (len + 1);
+	cpy = a;
+	cpy += (len - 1);
+
+	/* Reverse string */
+	while (*s != '\0')
+	{
+		*s = *cpy;
+		s++;
+		cpy--;
+	}
+}
