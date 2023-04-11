dc61929bebb82c8322ff834e3eefd26dc66690c0
cnagyei <cnagyei15@gmail.com>
2023-04-10 23:51:01 -0700

    Reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,26 +9,27 @@
  */
 void rev_string(char *s)
 {
-	char a[1000];
-	char *cpy;	/* Pointer to copy string to */
-	int len;
+	char a[1000];		/* Large array - used as copy of s */
+	char *cpy;		/* Pointer to a, the array copy of s */
+	int len;		/* To determine length of s */
 	char *p = s;
 
-	cpy = &a[0];
+	cpy = &a[0];		/* Assign pointer to first element of a */
 
-	while (*p != '\0')
+	while (*p != '\0')	/* Determine the length of s */
 		p++;
 	len = p - s;
 
 	/* Create a copy */
-	while ((*cpy++ = *s++))
+	while ((*cpy++ = *s++))	/* Brings s '\0' plus 1, cpy to same */
 		;
 
-	/* Move s back to */
-	s -= (len + 1);
+	s -= (len + 1); 	/* Move s back to first element */
+
+	/* This section could also be coded as: cpy -= 2; */
 	cpy = a;
 	cpy += (len - 1);
-
+	
 	/* Reverse string */
 	while (*s != '\0')
 	{
