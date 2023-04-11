205ab6bf9aebeeadbd2cc466cf162f0e155ef5a0
cnagyei <cnagyei15@gmail.com>
2023-04-11 01:16:07 -0700

    Put every other

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae bibendum quam. Nam mattis aliquet mollis. Aliquam erat volutpat. Duis purus neque, porta sed sem quis, sollicitudin varius orci. Donec ut nisi id ex efficitur finibus eget eget dolor. Fusce purus metus, convallis sed imperdiet et, consequat nec ante. Donec id condimentum enim. Nullam efficitur sol";
+
+    puts2(str);
+    return (0);
+}
