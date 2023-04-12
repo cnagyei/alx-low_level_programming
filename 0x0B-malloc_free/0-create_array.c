7f4c81ddf6e69ac81381e25a9391528381c12744
cnagyei <cnagyei15@gmail.com>
2023-04-11 19:30:36 -0700

    Create arrary and initialize with char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,13 +18,13 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	
+
 	arr = malloc(sizeof(char) * size);
 	while (i < size)
 	{
 		*(arr + i) = c;			/* Same as arr[i] */
 		i++;
 	}
-	
+
 	return (arr);
 }
