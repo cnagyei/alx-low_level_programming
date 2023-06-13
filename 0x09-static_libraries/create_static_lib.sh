ce2a37e0b5986bdf4f125b0276ca829a3e017827
cnagyei <cnagyei15@gmail.com>
2023-06-12 19:49:20 -0700

    create library

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-ar -rc liball.a ./*.c
+
+# compile source code into object file ending with .o
+gcc -c ./*.c
+
+# archive the object files into a library
+ar -rc liball.a ./*.o
+
+# index the library
+ranlib liball.a
