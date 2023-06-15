fe2ad7612648221b022a7af5e73cecd01a0dfbd1
cnagyei <cnagyei15@gmail.com>
2023-06-15 09:27:42 -0700

    allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: total bytes to allocate
+ *
+ * Return: pointer to allocated memory, otherwise
+ * normal process termination with a status value of 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem;
+
+	if (b == 0)
+		return (NULL);
+
+	mem = malloc(b);
+
+	if (mem == NULL)
+		exit(98);
+	
+	return (mem);
+	free(mem);
+}
