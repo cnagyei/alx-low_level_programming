d3828881d1f008fba93d477e1a9d7afa52342b0a
cnagyei <cnagyei15@gmail.com>
2023-06-19 17:21:44 -0700

    print a struct dog

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-    struct dog my_dog;
+	struct dog my_dog;
 
-    my_dog.name = NULL;
-    my_dog.age = 0;
-    my_dog.owner = "Bob";
-    print_dog(&my_dog);
-    return (0);
+	my_dog.name = NULL;
+	my_dog.age = 0;
+	my_dog.owner = "Bob";
+	print_dog(&my_dog);
+	return (0);
 }
