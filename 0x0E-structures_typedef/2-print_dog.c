a530292efa049efe7adb95b175edb27949e96f1f
cnagyei <cnagyei15@gmail.com>
2023-06-19 17:30:45 -0700

    print a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,19 +10,23 @@
  */
 void print_dog(struct dog *d)
 {
+	/* check if struct variable is null */
 	if (d == NULL)
 		exit(EXIT_SUCCESS);
 
+	/* print name if not null */
 	if (d->name == NULL)
 		printf("Name: \"(nil)\"\n");
 	else
 		printf("Name: %s\n", d->name);
 
+	/* print age if not null - 0 */
 	if (d->age == 0)
 		printf("Age: \"(nil)\"\n");
 	else
 		printf("Age: %f\n", d->age);
 
+	/* print owner if not null */
 	if (d->owner == NULL)
 		printf("Owner: \"(nil)\"\n");
 	else
