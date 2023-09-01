98462fdf5665ec63ffe6012b819746e3c5d2358e
cnagyei <cnagyei15@gmail.com>
2023-08-31 19:43:10 -0700

    binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] ; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[i] < 48 || b[i] > 49)
 			return (0);
 		sum = 2 * sum + (b[i] - '0');
 	}
