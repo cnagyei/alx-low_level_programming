7e537ca4f8ef436e1e5e006aa10d4f6d29b13cbe
cnagyei <cnagyei15@gmail.com>
2023-09-05 00:05:38 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -35,6 +35,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+		text_content = "";
+
 	/* open a filename, create it if not exist */
 	fd_dest = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd_dest == -1)
