dc721afd92419e7727e3ca6f0d78eef4b502412f
cnagyei <cnagyei15@gmail.com>
2023-09-13 11:07:54 -0700

    copy to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,25 +1,76 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string.
+ * error_fd_source - checks for error for source file
+ * @fd_source: file descriptor for source file
+ * @argv: argument vector
  *
- * @s: Pointer to the variable holding the string.
- *
- * Return: Length of string as int.
+ * Return: Void
  */
-size_t _strlen(char *s)
+void error_fd_source(int fd_source, char **argv)
 {
-	size_t len = 0;
+	if (fd_source == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+				argv[1]);
+		exit(98);
+	}
+}
 
-	for ( ; *s != '\0'; s++)
-		len++;
-	return (len);
+/**
+ * error_fd_dest - checks for error for destination file
+ * @fd_dest: file descriptor for destination file
+ * @argv: argument vector
+ *
+ * Return: Void
+ */
+void error_fd_dest(int fd_dest, char **argv)
+{
+	if (fd_dest == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * error_read_source - checks for error for destination file
+ * @read_source: file descriptor for destination file
+ * @argv: argument vector
+ *
+ * Return: Void
+ */
+void error_read_source(int read_source, char **argv)
+{
+	if (read_source == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+				argv[1]);
+		exit(98);
+	}
+}
+
+/**
+ * error_write_dest - checks for error for destination file
+ * @write_dest: file descriptor for destination file
+ * @argv: argument vector
+ *
+ * Return: Void
+ */
+void error_write_dest(int write_dest, char **argv)
+{
+	if (write_dest == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
 }
 
 /**
  * main - copies the content of a file to another file
- * @argc - count of command line arguments
- * @argv - string array of command line arguments
+ * @argc: count of command line arguments
+ * @argv: string array of command line arguments
  *
  * Return: 1 on success
  */
@@ -27,7 +78,7 @@ int main(int argc, char **argv)
 {
 	int fd_source, fd_dest, close_source, close_dest;
 	size_t len;
-	ssize_t read_from_source, write_to_dest;
+	ssize_t read_source, write_dest;
 	char *buffer;
 
 	if (argc != 3)
@@ -37,37 +88,19 @@ int main(int argc, char **argv)
 	}
 	/* open source file into file descriptor table  - should be O_RDONLY*/
 	fd_source = open(argv[1], O_RDONLY);
-	if (fd_source == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-				argv[1]);
-		exit(98);
-	}
+	error_fd_source(fd_source, argv);
 	/* open destination file into file descriptor table */
 	fd_dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_dest == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+	error_fd_dest(fd_dest, argv);
 	buffer = malloc(sizeof(char) * 1024);
 	/* read content from fd_source into buffer */
-	read_from_source = read(fd_source, buffer, 1024);
-	if (read_from_source == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-				argv[1]);
-		exit(98);
-	}
+	read_source = read(fd_source, buffer, 1024);
+	error_read_source(read_source, argv);
 	/* check length of source file */
 	len = _strlen(buffer);
 	/* write content from souce file into destination file in fd table*/
-	write_to_dest = write(fd_dest, buffer, len);
-	if (write_to_dest == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+	write_dest = write(fd_dest, buffer, len);
+	error_write_dest(write_dest, argv);
 	close_source = close(fd_source);
 	if (close_source == -1)
 	{
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,5 +11,25 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error_fd_source(int fd_source, char **argv);
+void error_fd_dest(int fd_dest, char **argv);
+void error_read_source(int read_source, char **argv);
+void error_write_dest(int write_dest, char **argv);
+
+/**
+ * _strlen - Returns the length of a string.
+ *
+ * @s: Pointer to the variable holding the string.
+ *
+ * Return: Length of string as int.
+ */
+size_t _strlen(char *s)
+{
+	size_t len = 0;
+
+	for ( ; *s != '\0'; s++)
+		len++;
+	return (len);
+}
 
 #endif /* MAIN_H */
