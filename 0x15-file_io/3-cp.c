8dc47db674727ac266b4f2ef967fa5736cf568e0
cnagyei <cnagyei15@gmail.com>
2023-09-13 09:46:08 -0700

    copy to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -48,7 +48,7 @@ int main (int argc, char **argv)
 	}
 
 	/* open destination file into file descriptor table */
-	fd_dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 00664);
+	fd_dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_dest == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
