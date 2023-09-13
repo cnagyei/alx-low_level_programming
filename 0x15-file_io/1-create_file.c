24c8087839853bf1a5e5110ce36920b612c34ad4
cnagyei <cnagyei15@gmail.com>
2023-09-13 03:50:16 -0700

    update create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -47,7 +47,7 @@ int create_file(const char *filename, char *text_content)
 	len = _strlen(text_content);
 
 	/* write from buffer to filename */
-	count_write = write(fd, text_content, (len + 1));
+	count_write = write(fd, text_content, (len));
 	if (count_write == -1)
 		return (-1);
 
