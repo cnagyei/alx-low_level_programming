77d08908e8e6b4c18bc3ef01a327dfcc2d900783
cnagyei <cnagyei15@gmail.com>
2023-09-28 19:36:07 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,32 +8,38 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int sum = 2;
-	int i, count = 0;
+	unsigned long int sum;
+	int i, j, cum = 0;
 
 	if (n == 0)
-	{
 		_putchar('0');
-		EXIT_SUCCESS;
-	}
 	if (n == 1)
-	{
 		_putchar('1');
-		EXIT_SUCCESS;
-	}
-	while (sum <= n)
+	i = 1;
+	sum = 2;
+	while ((sum * 2) <= n)
 	{
-		sum = sum * 2;
-		count++;
+		sum *= 2; /* move 2 to the highest power */
+		i++;
 	}
-	if ((n - sum) == 0)
+	/* check from higest power and descend */
+	while ((n != 0) && (n != 1) && (i >= 0))
 	{
-		_putchar('1');
-		for (i = 0; i < (count - 1); i++)
-			_putchar('0');
+		sum = 2;
+		for (j = i ; j > 1; j--)
+			sum *= 2; /* calculate powers of 2 */
+		if (i == 1)
+			sum = 2;
+		if (i == 0)
+			sum = 1;
+		/* accumulate total and check if equal or less than n */
+		if ((cum + sum) <= n)
+		{
+			cum += sum;
+			_putchar('1');
+		}
+		else
+			_putchar ('0');
+		i--;
 	}
-	/*
-	else
-		print_binary(n - sum);
-	*/
 }
