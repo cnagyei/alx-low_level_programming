556003a5cc4e6fdc98a16cde727f8087ee607f05
cnagyei <cnagyei15@gmail.com>
2023-10-02 15:35:39 -0700

    create file

--- c17-file_io/1-create_file.c
+++ c17-file_io/1-create_file.c
@@ -46,7 +46,7 @@ int create_file(const char *filename, char *text_content)
 	/* find bytes in text_content */
 	len = _strlen(text_content);
 
-	/* write from buffer to filename */
+	/* write from text_content to filename */
 	count_write = write(fd, text_content, len);
 	if (count_write == -1)
 		return (-1);
